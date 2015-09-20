fefe7e629aad58f1ad3bf1c43bd9d5c0f9b509b6
chapal <siarhei.sharykhin@gmail.com>
2015-09-20 21:54:52 +0300

    add BooKTableRow and BookTable components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/*var SearchPanel = React.createClass({
+var SearchPanel = React.createClass({
 	render: function() {
 		return (
 			<div className="row">
@@ -17,7 +17,45 @@
 		var query = React.findDOMNode(this.refs.search).value;
 		this.props.onSearchChanged(query);
 	}
-});*/
+});
+
+var BookTableRow = React.createClass({
+	render: function() {
+		return (
+			<tr>
+				<td>{this.props.book.title}</td>
+				<td>{this.props.book.category}</td>
+				<td><a href='#' onClick={this.onClick}>Edit</a></td>
+			</tr>
+			);
+	},
+	onClick: function(id) {
+		this.props.handleEditClickPanel(this.props.book.id);
+	}
+});
+
+
+var BookTable = React.createClass({
+	render: function() {
+		var rows = [];
+		this.props.books.forEach(function(book) {
+			rows.push(<BookTableRow key={book.id} book={book} handleEditClickPanel={this.props.handleEditClickPanel} />);
+		}.bind(this));
+
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Category</th>
+						<th>Edit</th>
+					</tr>
+				</thead>
+				<tbody>{rows}</tbody>
+			</table>
+			);
+	}
+});
 
 var HelloWorld = React.createClass({
 	render: function() {
