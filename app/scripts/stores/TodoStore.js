9158b8c84d877abba17830ccb36e80d80cfc30de
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-10-13 08:31:59 +0300

    change serve path

--- app/scripts/stores/TodoStore.js
+++ app/scripts/stores/TodoStore.js
@@ -5,8 +5,14 @@ var extend = require('../utils/extend');
 
 var CHANGE_EVENT='change';
 
+var _todos = {};
+
 TodoStore = function() {};
 
 extend(EventEmitter,TodoStore);
+
+TodoStore.prototype.getAll = function() {
+	return _todos;
+}
 
 module.exports = TodoStore;
